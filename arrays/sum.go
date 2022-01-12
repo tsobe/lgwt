912f0f055ef7cd82033cbbbbd8a3ddd65272cad7
Beka Tsotsoria <beka.tsotsoria@hellofresh.com>
2022-01-12 19:02:01 +0100

    Implemented tail summation

--- arrays/sum.go
+++ arrays/sum.go
@@ -15,3 +15,16 @@ func SumAll(matrix ...[]int) []int {
 	}
 	return sums
 }
+
+func SumAllTails(matrix ...[]int) []int {
+	var tailSums []int
+	for _, numbers := range matrix {
+		if len(numbers) == 0 {
+			tailSums = append(tailSums, 0)
+		} else {
+			tail := numbers[1:]
+			tailSums = append(tailSums, Sum(tail))
+		}
+	}
+	return tailSums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -12,7 +12,7 @@ func TestSum(t *testing.T) {
 	got := Sum(numbers)
 
 	if got != want {
-		t.Errorf("Got %d, expected %d. Numbers: %v", got, want, numbers)
+		t.Errorf("Got %d, expected %d. Input: %v", got, want, numbers)
 	}
 }
 
@@ -24,6 +24,36 @@ func TestSumAll(t *testing.T) {
 	got := SumAll(numbers1, numbers2)
 
 	if !reflect.DeepEqual(got, want) {
-		t.Errorf("Got %v, expected %v", got, want)
+		t.Errorf("Got %v, expected %v. Input: %v", got, want, [][]int{numbers1, numbers2})
 	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	assertTailSums := func(t *testing.T, got []int, want []int, input ...[]int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v, expected %v. Input: %v", got, want, input)
+		}
+	}
+
+	t.Run("Sum all tails of non-empty slices", func(t *testing.T) {
+		numbers1 := []int{1, 2, 10, 4}
+		numbers2 := []int{1, 8}
+		want := []int{16, 8}
+
+		got := SumAllTails(numbers1, numbers2)
+
+		assertTailSums(t, got, want, numbers1, numbers2)
+	})
+
+	t.Run("Sum all tails of empty slices", func(t *testing.T) {
+		numbers1 := []int{}
+		numbers2 := []int{2, 7}
+		want := []int{0, 7}
+
+		got := SumAllTails(numbers1, numbers2)
+
+		if !reflect.DeepEqual(got, want) {
+			assertTailSums(t, got, want, numbers1, numbers2)
+		}
+	})
 }
