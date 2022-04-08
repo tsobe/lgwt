5250ca05f0a63c5ab656febd54db235a27e63f45
Beka Tsotsoria <beka.tsotsoria@hellofresh.com>
2022-04-08 17:32:06 +0200

    Implemented basic blogs reading test

---
+++ blogposts/blogposts_test.go
@@ -0,0 +1,39 @@
+package blogposts_test
+
+import (
+	"io/fs"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("hi")},
+		"hello-world2.md": {Data: []byte("hola")},
+	}
+
+	posts, err := NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
+
+type Post struct {
+}
+
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
+	var posts []Post
+	for range dir {
+		posts = append(posts, Post{})
+	}
+	return posts, nil
+}
