b289a99e99d0e1846220952fe9a879cd721d50a1
Beka Tsotsoria <beka.tsotsoria@hellofresh.com>
2022-04-19 16:14:10 +0200

    Added main executable package

---
+++ blogposts/cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/tsobe/blogposts"
+	"log"
+	"os"
+)
+
+func main() {
+	posts, err := blogposts.NewPostsFromFS(os.DirFS("posts"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(posts)
+}
---
+++ blogposts/cmd/posts/none.md
@@ -0,0 +1,6 @@
+Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World
